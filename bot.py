379bf364cd5da59b26ed57e2a3c6f66e62770d99
anthony5000 <66759542+anthony5000@users.noreply.github.com>
2020-06-11 12:02:42 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ import inspect
 from contextlib import redirect_stdout
 from discord.ext import commands
 import json
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('+'), description="A selfbot made by L3NNY#0849\n\nHelp Commands", owner_id=411683912729755649)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'), description="Selfbot working!\n\nHelp Commands", owner_id=705123069014048911)
 
 
 @bot.command()
